58d56ed6981026425b2d4d312badd54be8f9452d
Shan Ul Haq <g@grevolution.me>
2014-03-15 09:47:38 +0800

    Initial Commit

---
+++ SHLineGraphView/SHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHAppDelegate.h
+//  SHLineGraphView
+//
+//  Created by SHAN UL HAQ on 15/3/14.
+//  Copyright (c) 2014 grevolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SHLineGraphView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SHLineGraphView
+//
+//  Created by SHAN UL HAQ on 15/3/14.
+//  Copyright (c) 2014 grevolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([SHAppDelegate class]));
+  }
+}
